2710fa75df12bdf51a2b5a4f4dcffbb55b9602ba
kosikhar <kosikhar@ualberta.ca>
2017-03-04 10:47:03 -0700

    Apparently previous commit didnt have the Headers

---
+++ Atmel Studio/Diode Temp Sensor/LCD_Write.c
@@ -0,0 +1,7 @@
+/*
+ * LCD_Write.c
+ *
+ * Created: 2017-03-04 10:13:34 AM
+ *  Author: Koltin Kosik-Harvey
+ */ 
+
---
+++ Atmel Studio/Diode Temp Sensor/LCD_Write.h
@@ -0,0 +1,18 @@
+/*
+ * LCD_Write.h
+ *
+ * Created: 2017-03-04 10:13:08 AM
+ *  Author: Koltin Kosik-Harvey
+ */ 
+
+
+#ifndef LCD_WRITE_H_
+#define LCD_WRITE_H_
+
+
+#include <lcd.h>
+
+
+
+
+#endif /* LCD_WRITE_H_ */
